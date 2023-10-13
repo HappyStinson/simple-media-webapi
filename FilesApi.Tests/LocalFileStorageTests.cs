cb201b64c0e12e63e460065d90004bda789f60f0
Rasmus Nordling <rasmus.nordling@gmail.com>
2023-10-13 18:19:35 +0200

    More tests to LocalFileStorage

--- FilesApi.Tests/LocalFileStorageTests.cs
+++ FilesApi.Tests/LocalFileStorageTests.cs
@@ -1,14 +1,80 @@
-using FilesApi.Controllers;
+using Microsoft.AspNetCore.Http;
+using Moq;
 
 public class LocalFileStorageTests
 {
-    // Task<bool> SaveFileAsync(IFormFile file);
+    [Fact]
+    public async Task SaveFileAsync_WhenFileTooLarge_ReturnsFalse()
+    {
+        // Arrange
+        var storage = new LocalFileStorage("media-test");
+        var file = new Mock<IFormFile>();
+        file.Setup(f => f.Length).Returns(600 * 1048576); // Simulate a file larger than 500MB.
+
+        // Act
+        var result = await storage.SaveFileAsync(file.Object);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task SaveFileAsync_WhenFileExistsAndNotUpdated_ReturnsFalse()
+    {
+        // Arrange
+        var storagePath = "media-test";
+        var fileName = "existingFile.txt";
+        var filePath = Path.Combine(storagePath, fileName);
+
+        // Create a file with the same name and length as the input file
+        File.WriteAllText(filePath, "Some content");
+        
+        var storage = new LocalFileStorage(storagePath);
+        var file = new Mock<IFormFile>();
+        file.Setup(f => f.FileName).Returns(fileName);
+        file.Setup(f => f.Length).Returns(new FileInfo(filePath).Length);
+
+        // Act
+        var result = await storage.SaveFileAsync(file.Object);
+
+        // Assert
+        Assert.False(result);
+
+        // Clean up the file created during the test
+        File.Delete(filePath);
+    }
+
+    [Fact]
+    public async Task SaveFileAsync_WhenFileDoesNotExist_UploadsFileAndReturnsTrue()
+    {
+        // Arrange
+        var storagePath = "media-test";
+        var fileName = "newFile.txt";
+        var filePath = Path.Combine(storagePath, fileName);
+
+        var storage = new LocalFileStorage(storagePath);
+        var file = new Mock<IFormFile>();
+        file.Setup(f => f.FileName).Returns(fileName);
+        file.Setup(f => f.Length).Returns(100); // File size within the limit
+
+        // Act
+        var result = await storage.SaveFileAsync(file.Object);
+
+        // Assert
+        Assert.True(result);
+
+        // Verify that the file was created
+        Assert.True(File.Exists(filePath));
+
+        // Clean up the file created during the test
+        File.Delete(filePath);
+    }
 
     [Fact]
     public void GetFileList_EmptyDirectoryShouldWork()
     {
         // Arrange
-        LocalFileStorage storage = new("media-empty");
+        LocalFileStorage storage = new("media-test");
         List<FileMetadata> expected = new();
 
         // Act
